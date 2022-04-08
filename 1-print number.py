b09ccd9fc3d10f880bf24186842bf40235038fc1
parsa-eghbali <103234313+parsa-eghbali@users.noreply.github.com>
2022-04-08 08:51:35 +0430

    Add files via upload
    learn-print number in python
    اموزش خروجی گرفتن از اعداد در پایتون

---
+++ 1-print number.py
@@ -0,0 +1,16 @@
+#basic ---- ساده
+
+#you can make list or input number///////////  number:integer - flot - complex
+#get number --->make(list,tuple,set,dictionary) or int(input("number:"))  / int=integer
+
+number=[1,2,3,4,5,6,7,8,9]# [] ----> list
+print("number=",number)
+
+print("-"*40)
+
+number2=int(input("number="))
+print(number2)
+
+
+#میشه از لیست و input برای گرفتن عدد استفاده کرد
+#اعداد را نی توان در (لیست،تاپل،ست،دیکشنری) یا از  int(input("number")) برای گرقتن عدد از کاربر
