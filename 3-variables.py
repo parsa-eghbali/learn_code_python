4b679822166d9d0f641b56790723ade201439970
parsa-eghbali <103234313+parsa-eghbali@users.noreply.github.com>
2022-04-09 11:22:31 +0430

    Add files via upload

---
+++ 3-variables.py
@@ -0,0 +1,26 @@
+#In Python, variables are created when you assign a value to it:
+
+# 1
+x=2 # x its variable
+y=3 # y its variable
+print(x,y)
+
+print("-"*40)
+
+# 2
+a,b="oo","pp"
+print(a,b)
+
+print("-"*40)
+
+# 3
+c=int(input("nume:"))
+d=int(input("nume:"))
+print(c,d)
+
+print("-"*40)
+
+m,M=5,4 #big F deffrent f  
+print(m,M)
+
+#برای ساخت متغییر در پایتون یک نام سپس از = برای انتساب استفاده می کنیم یعنی اون چیز رو به رو را در متغییر بریز
