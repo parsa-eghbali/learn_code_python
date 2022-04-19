7550693048f925cf0a9f12fbf3b4343ce89665d2
parsa-eghbali <103234313+parsa-eghbali@users.noreply.github.com>
2022-04-19 15:30:41 +0430

    عملگرهای مقایسه ای (Comparison Operators)
    عملگرهای مقایسه ای (Comparison Operators)

---
+++ 5-operators(moghayse).py
@@ -0,0 +1,32 @@
+#basic ---- ساده
+#Comparative operators
+#عملگر های مقایسه ای
+# == , > , < ,<= , >= , !=
+
+# =
+x,y=5,7
+print(x == y)
+ 
+# >
+x,y=5,7
+print(x > y)
+
+# <
+x,y=5,7
+print(x < y)
+
+# <=
+x,y=5,7
+print(x <= y)
+
+# >=
+x,y=5,7
+print(x >= y)
+
+# !=
+x,y=5,7
+print(x != y)
+
+i="football"
+j="python"
+print(i != j)
